b0331abc2b073051c5d5ab4710923398ced2d3b4
hoodielive <hoodielive@gmail.com>
2019-11-10 19:08:22 -0500

    duh..

--- Configuration/provision.sh
+++ Configuration/provision.sh
@@ -1,84 +1,84 @@
-#!/bin/bash 
-set -e 
+#!/bin/bash
+set -e
 set -x
 
 unset OS_RELEASE
 unset OS
-J
+
 OS_RELEASE="/etc/os-release"
 OS=""
 
 # First figure out what system this is redhat, debian, suse, arch, gentoo.
 
-if fgrep -q -i debian $OS_RELEASE; then
+if egrep -q -i debian $OS_RELEASE; then
 	OS="debian"
-elif fgrep -q -i redhat /etc/os-release; then 
+elif egrep -q -i redhat /etc/os-release; then
 	OS="redhat"
-elif fgrep -q -i ubuntu /etc/os-release; then 
+elif egrep -q -i ubuntu /etc/os-release; then
 	OS="ubuntu"
-elif fgrep -q -i opensuse /etc/os-release; then 
+elif egrep -q -i opensuse /etc/os-release; then
 	OS="opensuse"
-elif fgrep -q -i freebsd /etc/*-release; then 
+elif egrep -q -i freebsd /etc/*-release; then
 	OS="freebsd"
 else
 	UNPROVISION="true"
-fi 
+fi
 
 # FIXME
-# Perhaps we should determine that in the case there is an instance of debian, we should ascertain specifically what 
-# flavor of debian we are provisioning. 
+# Perhaps we should determine that in the case there is an instance of debian, we should ascertain specifically what
+# flavor of debian we are provisioning.
 
-# Then check disk space and memory 
+# Then check disk space and memory
 
-# Update the System and Validate it is completely up-to-date. 
-# Remove broken symlinks and dependencies 
+# Update the System and Validate it is completely up-to-date.
+# Remove broken symlinks and dependencies
 
 
 # Each distro has development tools - make sure to install them along with clang.
 
-# Read in a file that lists the packages you want installed on the system. 
+# Read in a file that lists the packages you want installed on the system.
 
-# First verify that all 3rd party repos are up-to-date. 
+# First verify that all 3rd party repos are up-to-date.
 
 # Configurations can be a part of a case statement.
 
-# ZSH Configuration 
+# ZSH Configuration
 if zsh is installed  - How would you test if zsh is installed ?
     curl oh-my-zsh  - Yes but is curl installed because on ubuntu it is not installed by default.
-if zsh is not installed 
+if zsh is not installed
     install it  - Yes but is the Logic present to install based upon distro variant?
     && and set zhann as the default zsh environment
-    source it && 
-    set zsh as the default shell for user or perhaps globally for ALL users 
+    source it &&
+    set zsh as the default shell for user or perhaps globally for ALL users
 fi
 
 install neovim, vim and emacs
 
-# rbenv Configuration 
-if the build tools are installed 
-    git clone rbenv and ruby-build 
-if build tools are not installed 
-    install build tools 
+# rbenv Configuration
+if the build tools are installed
+    git clone rbenv and ruby-build
+if build tools are not installed
+    install build tools
     git clone rbenv and ruby-build
 fi
 
-if ruby is installed 
-    if it is 2.6.3 
+if ruby is installed
+    if it is 2.6.3
         continue
-    else 
+    else
         rbenv install 2.6.3
         rbenv global 2.6.3
 fi
 
 # Is gem installed?
-if gem is installed 
-    echo "The gem home environment is " gem env home function 
-    gem install bundler 
-    gem install rails 
-    gem install pry 
+if gem is installed
+    echo "The gem home environment is " gem env home function
+    gem install bundler
+    gem install rails
+    gem install pry
     gem install neovim
 else
-    echo "gem is not installed." && install it && run test again 
+    echo "gem is not installed." && install it && run test again
 
 
 # NodeJS
@@ -86,70 +86,70 @@ curl nvm and install v12
 npm install -g cli-highlight, create-react-app, express generator, typescript, neovim
 
 
-# Python3 
+# Python3
 if python3 is installed
     if pip3 is installed
-        pip3 install neovim, bpython, ptpython, ptipython 
-if python3 is not installed 
-    $PACKAGE_MANAGER install python3, pip3 
-        pip3 install neovim, bpython, ptpython, ptipython 
+        pip3 install neovim, bpython, ptpython, ptipython
+if python3 is not installed
+    $PACKAGE_MANAGER install python3, pip3
+        pip3 install neovim, bpython, ptpython, ptipython
 fi
 
 # Csharp
 install csharp (mono)
 
-# Golang 
-install golang 
+# Golang
+install golang
 
-# Rust 
-install rustup 
+# Rust
+install rustup
 
-# Haskell 
-install haskell 
+# Haskell
+install haskell
 
-# Racket 
-install racket 
+# Racket
+install racket
 
-# Crystal 
-install crystal 
+# Crystal
+install crystal
 
-# Erlang 
-install erlang 
+# Erlang
+install erlang
 
-# Elixir 
+# Elixir
 install elixir
 
 # Perl6
 install perl6
 
-# PHP / Composer 
+# PHP / Composer
 install php7 composer laravel
 
-# Virtualization 
-install VirtualBox, Vagrant, Terraform, Docker and Habor 
+# Virtualization
+install VirtualBox, Vagrant, Terraform, Docker and Habor
 
-# Graphics / Video / 3D 
+# Graphics / Video / 3D
 blender3d, natron, lightworks, gimp, inkscape, scribus
 
-# API 
+# API
 httpie, jq
 
-# Parser and Regex support 
+# Parser and Regex support
 ripgrep, sift, ag, ack, pt, ucg, git grep
 
-# Email Support 
+# Email Support
 neomutt, thunderbird or evolution
 
-# Windows apps on linux 
+# Windows apps on linux
 playonlinux
 
-# IDE 
-monodevelop 
+# IDE
+monodevelop
 
-# visualcode 
-curl 
-extract 
-install 
+# visualcode
+curl
+extract
+install
 code install plugins
 
 # visual studio (unity)
@@ -158,34 +158,34 @@ just install
 # rider (unity)
 just install
 
-# monodevelop ide 
+# monodevelop ide
 just install
 
-# neovim configuration 
+# neovim configuration
 make .config/nvim/init.vim
-import basic neovim configuration and inoremap jj <ESC>  
-update remote 
+import basic neovim configuration and inoremap jj <ESC> 
+update remote
 pluginstall
-plugupdate 
+plugupdate
 plugclean
 
 # doom emacs configuration
 emacs vs25-26
-clone doom 
-change to development branch 
+clone doom
+change to development branch
 quickstart
 
-# Fonts 
-mono dank 
+# Fonts
+mono dank
 fira code
-fc-cache -fv 
+fc-cache -fv
 
-# Icons 
-paper themes 
+# Icons
+paper themes
 
-# Themes 
+# Themes
 
-# Wallpaper 
-    # Low Poly 
+# Wallpaper
+    # Low Poly
     # Unsplash
 
